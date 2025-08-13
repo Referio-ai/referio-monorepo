4ff4d1a3f109cb8990ee6a2c2020187d0a46fa23
ralphevano <evanoralph@gmail.com>
2025-08-13 22:43:49 +0800

    Fix bugs and loadings

--- apps/web/constants/appNavigation.tsx
+++ apps/web/constants/appNavigation.tsx
@@ -3,7 +3,7 @@ import { BarChart3, Building, ClipboardList, Home, QrCode, Settings, Users, List
 export const appNavigation = [
   { icon: Home, label: 'Dashboard',  path: '/referral-management/' },
   { icon: QrCode, label: 'Referrals',  path: '/referral-management/referral-management/' },
-  { icon: ListChecksIcon, label: 'Referrals-list', path: '/referral-management/referral-list/' },
+  // { icon: ListChecksIcon, label: 'Referrals-list', path: '/referral-management/referral-list/' },
   { icon: Building, label: 'Facilities', path: '/referral-management/facilities/' },
   { icon: UserCheck, label: 'Facilitators', path: '/referral-management/facilitator/' },
   { icon: Users, label: 'Patients',  path: '/referral-management/patients/' },
